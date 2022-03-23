4160ddf7e38ac799610748c7f9ebe1a605ed9b14
chughes3393 <hughes.c33@gmail.com>
2022-03-23 15:32:51 -0400

    working

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import './styles.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Home = () => {
-    return (
-        <div>
-            <div id='home-div'>
-                
-                <h2>“Considering how dangerous everything is, nothing is really very frightening.”</h2>
-                <h4>- Gertrude Stein</h4>
+
+  var ReactRotatingText = require('react-rotating-text');
+
+  const [quotes, setQuotes] = useState([])
+
+  useEffect(() => {
+    fetchQuotes()
+  }, [])
+
+  const fetchQuotes = async () => {
+    try {
+      const response = await axios.get('http://localhost:8081/api/v1/portfolio-back/allquotes')
+
+      // console.log(response)
+      setQuotes(response.data)
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+
+    <div>
+      {
+        quotes.map(quotes => {
+          return (
+            <div className='mt-5'>
+            <center>
+            <ReactRotatingText 
+            items={[
+              `${quotes.quote} ${quotes.from1}`,
+             `${quotes.quote} ${quotes.from1}`, 
+             `${quotes.quote} ${quotes.from1}`
+            ]} 
+             />
+              </center>
             </div>
-        </div>
-    );
+          )
+        })
+      }
+
+    </div>
+
+  );
 }
+
 
 export default Home;
--- src/pages/webdesign/index.js
+++ src/pages/webdesign/index.js
@@ -1,7 +1,8 @@
 import './styles.css'
 
 import placeholder from '../../photos/placeholder.png'
-
+import Slimetooth from '../../photos/slimetooth.png'
+import Logo from '../../photos/Logo.png'
 
 const WebDesign = () => {
 
@@ -13,7 +14,7 @@ const WebDesign = () => {
             <div className="card mb-3 mt-3" id='card1' >
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={placeholder} className="img-fluid rounded-start" alt="..." />
+                        <img src={Logo} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
@@ -28,13 +29,13 @@ const WebDesign = () => {
             <div className="card mb-3" id='card2' >
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={placeholder} className="img-fluid rounded-start" alt="..." />
+                        <img src={Slimetooth} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
                             <h5 className="card-title">Slimetooth Art</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <p className="card-text">Portfolio created for Slimetooth Art a business specializing in art and jewelry. </p>
+                            <p className="card-text"><small >Link</small></p>
                         </div>
                     </div>
                 </div>
